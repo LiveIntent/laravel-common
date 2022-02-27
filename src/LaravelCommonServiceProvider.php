13778325c2fad6ec26f0f23cf38510055fe5cc23
tam5 <arimiller92@gmail.com>
2022-02-27 16:24:20 -0500

    feat: register http logger

--- src/LaravelCommonServiceProvider.php
+++ src/LaravelCommonServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace LiveIntent\LaravelCommon;
 
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Database\Migrations\MigrationCreator;
+use Illuminate\Foundation\Http\Events\RequestHandled;
 use LiveIntent\LaravelCommon\Console\TestMakeCommand;
 use LiveIntent\LaravelCommon\Console\ModelMakeCommand;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -26,12 +28,13 @@ class LaravelCommonServiceProvider extends PackageServiceProvider implements Def
     {
         parent::register();
 
-        $this->registerHealthCheck();
-
         // override the migration's custom stub path
         $this->app->singleton('migration.creator', function ($app) {
             return new MigrationCreator($app['files'], __DIR__.'/Console/stubs');
         });
+
+        $this->registerHealthCheck();
+        $this->registerHttpLogger();
     }
 
     /**
@@ -71,6 +74,19 @@ class LaravelCommonServiceProvider extends PackageServiceProvider implements Def
     }
 
     /**
+     * Register http logger
+     */
+    protected function registerHttpLogger()
+    {
+        /** @psalm-suppress UndefinedClass */
+        if ($logger = config('liveintent.logging.logger')) {
+            $this->app['events']->listen(RequestHandled::class, [
+                new $logger(), 'logRequest',
+            ]);
+        }
+    }
+
+    /**
      * Register the health check endpoint.
      *
      * Override if need be by re-registering the route in your app.
@@ -78,5 +94,7 @@ class LaravelCommonServiceProvider extends PackageServiceProvider implements Def
     protected function registerHealthCheck()
     {
         Route::get('/health', fn () => response()->json());
+
+        Queue::looping(fn () => touch('/tmp/healthcheck'));
     }
 }
